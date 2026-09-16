59eefb1e45893dcfdba721cdd2646bddc33c3912
Prabin Kumar Khamania <prabinkumarkhamania7@gmail.com>
2026-09-16 23:42:50 +0530

    Add Bogosort [B] from Codeforces

---
+++ 1312/B - Bogosort/solution.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+ 
+    vector<int> arr(n);
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+ 
+    sort (arr.begin(), arr.end(), greater<int> ());
+ 
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+ 
+    cout << endl;
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t = 1;
+    cin >> t;
+ 
+    while (t--) {
+        solve();
+    }
+ 
+    return 0;
+}
